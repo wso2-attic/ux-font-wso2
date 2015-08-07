e9697afb520a1446c26dfd49f0746864ebcc7ea2
Jerad Rutnam <jeradrutnam@outlook.com>
2015-08-07 11:48:45 +0530

    Fixed an issue on gh-pages shell-script

--- gh-deploy.sh
+++ gh-deploy.sh
@@ -4,7 +4,7 @@ title Deploy gh-pages files to gh-pages brunch
 echo "gh-pages" folder contains will deploy to gh-pages brunch
 echo =========================================================
 echo please commit all the changes before continue
-echo.
+echo
 
 echo -n "Do you want to continue [Y/N]?"
 old_stty_cfg=$(stty -g)
@@ -12,16 +12,16 @@ stty raw -echo
 answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
 stty $old_stty_cfg
 if echo "$answer" | grep -iq "^y" ;then
-    echo.
+    echo
 	echo Deploying.....
-	echo.
+	echo
 	git pull
 	git subtree push --prefix gh-pages origin gh-pages
-	echo.
+	echo
 	echo Complete Deploying!
 	sleep
 else
-    echo.
+    echo
 	sleep
 fi
 exit 
